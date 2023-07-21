9e4f77fdaf5e2c3080c650ab7c47daa99fe21774
深度之恋 <130018882+Linux-qitong@users.noreply.github.com>
2023-07-21 16:03:18 +0800

    Add files via upload

---
+++ build_action.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+VERSION=$(grep 'Kernel Configuration' < config | awk '{print $3}')
+
+# add deb-src to sources.list
+sed -i "/deb-src/s/# //g" /etc/apt/sources.list
+
+# install dep
+sudo apt update
+sudo apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev git lsb vim libelf-dev
+sudo apt build-dep -y linux
+
+# change dir to workplace
+cd "${GITHUB_WORKSPACE}" || exit
+
+# download kernel source
+wget http://www.kernel.org/pub/linux/kernel/v6.x/linux-6.3.tar.gz  
+tar -xf linux-"$VERSION".tar.gz
+cd linux-"$VERSION" || exit
+
+# copy config file
+cp ../config .config
+
+# disable DEBUG_INFO to speedup build
+scripts/config --disable DEBUG_INFO
+scripts/config --set-str SYSTEM_TRUSTED_KEYS ""
+# apply patches
+# shellcheck source=src/util.sh
+# source ../patch.d/*.sh
+
+# build deb packages
+CPU_CORES=$(($(grep -c processor < /proc/cpuinfo)*2))
+sudo make bindeb-pkg -j"$CPU_CORES"
+
+# move deb packages to artifact dir
+cd ..
+mkdir "artifact"
+mv ./*.deb artifact/
