827a8162ea687711a7fdc590cbb07cccdf5e0146
青稚° <130018882+Linux-qitong@users.noreply.github.com>
2023-07-29 08:39:23 +0800

    Update build_action_zen.sh

--- build_action_zen.sh
+++ build_action_zen.sh
@@ -19,7 +19,7 @@ tar -xf v6.4.7-lqx1.tar.gz
 cd zen-kernel-6.4.7-lqx1|| exit
 
 # copy config file
-cp ../configdee .config
+cp ../configok .config
 
 # disable DEBUG_INFO to speedup build
 scripts/config --set-str SYSTEM_TRUSTED_KEYS ""
