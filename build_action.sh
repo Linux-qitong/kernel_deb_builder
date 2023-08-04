89c46ebabc153f45fde59de31e0ec24151825752
青稚° <130018882+Linux-qitong@users.noreply.github.com>
2023-08-04 16:54:38 +0800

    Update build_action.sh

--- build_action.sh
+++ build_action.sh
@@ -14,9 +14,9 @@ sudo apt build-dep -y linux
 cd "${GITHUB_WORKSPACE}" || exit
 
 # download kernel source
-wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.1.43.tar.xz
-tar -xf linux-6.1.43.tar.xz
-cd linux-6.1.43|| exit
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.4.6.tar.xz
+tar -xf linux-6.4.6.tar.xz
+cd linux-6.4.6|| exit
 
 # copy config file
 cp ../configf .config
