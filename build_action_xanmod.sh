6765228bce1a0802cf8d47e39481179a7d0d8725
青稚° <130018882+Linux-qitong@users.noreply.github.com>
2024-01-14 13:34:04 +0800

    Update build_action_xanmod.sh

--- build_action_xanmod.sh
+++ build_action_xanmod.sh
@@ -16,9 +16,9 @@ sudo apt build-dep -y linux
 cd "${GITHUB_WORKSPACE}" || exit
 
 # download kernel source
-wget https://gitlab.com/xanmod/linux/-/archive/6.6.7-xanmod1/linux-6.6.7-xanmod1.tar.gz
-tar -xf linux-6.6.7-xanmod1.tar.gz
-cd linux-6.6.7-xanmod1|| exit
+wget https://gitlab.com/xanmod/linux/-/archive/6.7.0-xanmod1/linux-6.7.0-xanmod1.tar.gz
+tar -xf linux-6.7.0-xanmod1.tar.gz
+cd linux-6.7.0-xanmod1|| exit
 
 # copy config file
 cp ../configsi .config
