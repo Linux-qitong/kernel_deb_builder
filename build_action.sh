c81623ba8cc2679bdc1add626ca5a6a30a8180c7
青稚° <130018882+Linux-qitong@users.noreply.github.com>
2024-02-06 10:11:09 +0800

    Update build_action.sh

--- build_action.sh
+++ build_action.sh
@@ -7,6 +7,7 @@ sed -i "/deb-src/s/# //g" /etc/apt/sources.list
 
 # install dep
 sudo dpkg -i *.deb
+sudo apt install -f
 sudo cp deepin-app-store-home.gpg /usr/share/keyrings/
 sudo cp deepin-archive-camel-keyring.gpg /usr/share/keyrings/
 sudo cp deepin-archive-uranus-keyring.gpg /usr/share/keyrings/
