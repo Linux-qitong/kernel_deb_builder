a7b261e5602a66e2a931958d6d980a0a21f40303
青稚 <130018882+Linux-qitong@users.noreply.github.com>
2025-02-21 18:29:43 +0800

    Update build_action.sh

--- build_action.sh
+++ build_action.sh
@@ -7,8 +7,8 @@ sed -i "/deb-src/s/# //g" /etc/apt/sources.list
 
 # install dep
 sudo apt update
-sudo apt install -y wget xz-utils make gcc-14 flex bison dpkg-dev bc rsync kmod cpio libssl-dev git lsb vim libelf-dev
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 100
+sudo apt install -y wget xz-utils make gcc-13 flex bison dpkg-dev bc rsync kmod cpio libssl-dev git lsb vim libelf-dev
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 100
 gcc -v
 sudo apt build-dep -y linux
 
